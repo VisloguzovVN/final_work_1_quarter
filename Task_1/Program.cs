ca82a80b73432a6cea5caf34cbaa45b566745a36
Viacheslav Visloguzov <VisloguzovVN@yandex.ru>
2024-02-24 13:30:38 +0800

    Change file

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -8,7 +8,7 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Введите элементы массива через пробел: ");
+        Console.Write("Пожалуйста введите элементы массива через пробел: ");
         string[] inputArray = Console.ReadLine()!.Split(' ');
 
         string[] resultArray = FilterArrayByLength(inputArray);
